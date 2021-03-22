001dbea7678403e22777980058578dbd356bd956
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-03-22 15:01:30 -0400

    deploying

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
-const FILES_TO_CACHE = [
+const FILES_TO_CACHE = [\
+  "/",
   "/index.html",
-  '/favicon.ico',
   "/db.js",
   "/index.js",
   "/manifest.webmanifest",
